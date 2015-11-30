c373e6babc551a977745e851da41505dc7340c03
Myers-Omri <omri.myers@gmail.com>
2015-11-30 16:21:28 -0500

    loop to find best hyperparams

--- neural_nets.py
+++ neural_nets.py
@@ -26,47 +26,63 @@ def net_class(ustraining_set, train_set_labels, usvalidation_set, validation_set
     net = buildNetwork(1024, 100, 8,outclass=SoftmaxLayer)
 
     ds = ClassificationDataSet(1024, 8, nb_classes=8)
-    trainer = BackpropTrainer(net, ds, learningrate=0.005, momentum=0, weightdecay=0.05, batchlearning=False,verbose=True)
     for d,t in zip(ltraining_set, train_set_labels):
         tarr = [int(i==t) for i in range(0,8)]
         ds.addSample(d, tarr)
 
-
-    #trainer.trainUntilConvergence()
     vds = ClassificationDataSet(1024, 8, nb_classes=8)
     for vd, vt in zip(lvalidation_set, validation_set_labels):
         vtarr = [int(i==vt) for i in range(0,8)]
         vds.addSample(vd, vtarr)
+    tot_min_err = 100.0
+    best_l = 0.0
+    best_w = 0.0
+    obest_e = 0
+    for l in [0.001, 0.002, 0.004, 0.005 ]:
+        for w in [0.05, 0.06, 0.7, 0.8, 0.9]:
+            net = buildNetwork(1024, 100, 8,outclass=SoftmaxLayer)
+            trainer = BackpropTrainer(net, ds, learningrate=l, momentum=0, weightdecay=w, batchlearning=False,verbose=True)
+            cmin_err = 100.0
+            flag = True
+            best_e = 0
+            e = 0
+            while flag:
+                flag = False
+                trnresult = 100.0
+                tstresult = 100.0
+                for i in range(20):
+                    e += 1
+                    trainer.trainEpochs(1)
 
-    min_err = 100.0
-    flag = True
-    while flag:
-        flag = False
-        trnresult = 100.0
-        tstresult = 100.0
-        for i in range(20):
-            trainer.trainEpochs(1)
-
-            trnresult = percentError( trainer.testOnClassData(),
-                                      train_set_labels )
+                    trnresult = percentError( trainer.testOnClassData(),
+                                              train_set_labels )
 
 
 
-            tstresult = percentError( trainer.testOnClassData(dataset=vds ), validation_set_labels )
-            if min_err >= tstresult:
-                min_err = tstresult
-                print "opt err ", tstresult
+                    tstresult = percentError( trainer.testOnClassData(dataset=vds ), validation_set_labels )
+                    if cmin_err >= tstresult:
+                        cmin_err = tstresult
+                        print "copt err ", tstresult
+                        best_e = e
+                        flag = True
+
+                # print "epoch: %4d" % trainer.totalepochs, \
+                #       "  train error: %5.2f%%" % trnresult, \
+                #    "  test error: %5.2f%%" % cmin_err
+
+            if tot_min_err > cmin_err:
+
+                tot_min_err = cmin_err
+
+                best_l = l
+                best_w = w
+                obest_e = best_e
+                print "new opt err:{}, for LR: {}, WD:{}, NE:{} ".format(tot_min_err, best_l, best_w, obest_e)
                 net.sorted = False
                 net.sortModules()
-                res_f = open('testNetwork.dump', 'w')
+                res_f = open('bestNet.dump', 'w')
                 pickle.dump(net,res_f )
                 res_f.close()
-                flag = True
-
-        print "epoch: %4d" % trainer.totalepochs, \
-              "  train error: %5.2f%%" % trnresult, \
-           "  test error: %5.2f%%" % min_err
-
 
 
 def load_net_and_check_errorate(X,Y):
