55e77f491cfe0a3cc715f9889b24f56c08f31ba9
David Jiang <davidjiang.haohan@gmail.com>
2015-11-30 16:43:31 -0500

    error with MoG

--- run_classifier.py
+++ run_classifier.py
@@ -5,6 +5,7 @@ import sklearn.linear_model
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import cross_validation
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.mixture import GMM
 import numpy as np
 
 
@@ -15,10 +16,6 @@ def standard_data(inputs):
 
 def knn(training_inputs, training_labels, valid_inputs, valid_label):
     knn_class = KNeighborsClassifier(weights='uniform', n_neighbors=5)
-
-    #training_inputs, valid_inputs, training_labels, valid_label = cross_validation.train_test_split(train_inputs, train_targets, test_size=0.3, random_state=0)
-    #Train knn
-    #standard_train_inputs = standard_data(training_inputs)
 
     standard_train_inputs = standard_data(training_inputs)
     standard_valid_inputs = standard_data(valid_inputs)
@@ -38,7 +35,7 @@ def logistic_regression(training_inputs, training_labels, valid_inputs, valid_la
 
 
 
-    logistic_regression_solver = sklearn.linear_model.LogisticRegression(penalty='l2', dual=False, tol=0.1, C=1.0, fit_intercept=True,
+    logistic_regression_solver = sklearn.linear_model.LogisticRegression(penalty='l2', dual=False, tol=0.01, C=1.0, fit_intercept=True,
                                                                          intercept_scaling=1, class_weight=None, random_state=None, solver='newton-cg',
                                                                          max_iter=100, multi_class='ovr', verbose=0, warm_start=False, n_jobs=2)
     standard_train_inputs = standard_data(training_inputs)
@@ -54,13 +51,42 @@ def logistic_regression(training_inputs, training_labels, valid_inputs, valid_la
     print accuracy
     #print "Accuracy for logistic regression is:{}".format(score_arr)
 
-def multi_naive_bayes(training_inputs, valid_inputs, training_labels, valid_label):
-    mnb = MultinomialNB(alpha=1.0, fit_prior=True, class_prior=None)
+def MoG(training_inputs, training_labels, valid_inputs, valid_label):
+    standard_train_inputs = standard_data(training_inputs)
+    standard_valid_inputs = standard_data(valid_inputs)    
 
+    n_classes = len(np.unique(training_labels))
 
+    #Try GMMs using different types of covariances.
+    classifiers = dict((covar_type, GMM(n_components=n_classes,
+                    covariance_type=covar_type, init_params='wc', n_init=10, n_iter=1000))
+                   for covar_type in ['spherical', 'diag', 'tied', 'full'])
 
-def neural_nets():
-    print ' '
+    n_classifiers = len(classifiers)
+
+    for index, (name, classifier) in enumerate(classifiers.items()):
+        # Since we have class labels for the training data, we can
+        # initialize the GMM parameters in a supervised manner.
+    
+        means = np.array([standard_train_inputs[training_labels == (i + 1)].mean(axis=0)
+                                      for i in xrange(n_classes)])
+
+        classifier.means_ = np.reshape(np.array([means[training_labels - 1] 
+                                for i in xrange(n_classes)]), (7, standard_train_inputs.shape[0]))
+
+        #print classifier.means_.shape[1]
+        #print standard_train_inputs.T.shape[1]
+        classifier.fit(standard_train_inputs.T)
+
+        y_train_pred = classifier.predict(standard_train_inputs.T)
+        train_accuracy = np.mean(y_train_pred.ravel() == training_labels.ravel()) * 100
+        
+        print train_accuracy
+
+        #y_test_pred = classifier.predict(valid_inputs.T)
+        #test_accuracy = np.mean(y_test_pred.ravel() == valid_label.ravel()) * 100
+
+        #print test_accuracy
 
 def get_prior_dist():
     stats = [0,0,0,0,0,0,0]
@@ -72,12 +98,10 @@ def get_prior_dist():
 
 if __name__ == '__main__':
     training_set, train_set_labels, validation_set, validation_set_labels = LoadData('labeled_images.mat', True, False)
-    #training_inputs, valid_inputs, training_labels, valid_label = cross_validation.train_test_split(train_inputs, train_targets, test_size=0.3, random_state=0)
-
+    #training_inputs, valid_inputs, training_labels, valid_label = cross_validation.train_test_split(train_inputs, train_targets, test_size=0.3, rlogistic_regression(training_set, train_set_labels, validation_set, validation_set_labels)
+    #knn(training_set, train_set_labels, validation_set, validation_set_labels)
     #logistic_regression(training_set, train_set_labels, validation_set, validation_set_labels)
-    knn(training_set, train_set_labels, validation_set, validation_set_labels)
-    #multi_naive_bayes(training_inputs, valid_inputs, training_labels, valid_label)
-    # neural_nets()
+    MoG(training_set, train_set_labels, validation_set, validation_set_labels)
 
 
 
